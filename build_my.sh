a6a7170369da8d3a51f5ac624e529330e69d0b09
Fedor917 <cryscript@gmail.com>
2017-01-22 23:50:39 +0700

    Add config

---
+++ build_my.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+export CONFIG_FILE="lineageos_z2_plus_defconfig"
+export ARCH="arm64"
+export CROSS_COMPILE="aarch64-linux-android-"
+export TOOL_CHAIN_PATH="/home/fedor917/android/aarch64-linux-android-4.9/bin"
+export CONFIG_ABS_PATH="arch/${ARCH}/configs/${CONFIG_FILE}"
+export PATH=$PATH:${TOOL_CHAIN_PATH}
+export objdir="${HOME}/kernel/zuk/obj"
+export sourcedir="/media/fedor917/Fedor917/android/RR-N/kernel/zuk/msm8996"
+compile() {
+  make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE}  $CONFIG_FILE -j4
+  make O=$objdir -j6
+}
+clean() {
+  make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE}  $CONFIG_FILE -j4
+  make O=$objdir mrproper
+}
+module_stock(){
+  rm -rf /${HOME}/kernel/zuk/anykernel/modules/
+  mkdir /${HOME}/kernel/zuk/anykernel/modules
+  find $objdir -name '*.ko' -exec cp -av {} /${HOME}/kernel/zuk/anykernel/modules/ \;
+  # strip modules
+ ${TOOL_CHAIN_PATH}/${CROSS_COMPILE}strip --strip-unneeded ${HOME}/kernel/zuk/anykernel/modules/*
+  mkdir /${HOME}/kernel/zuk/anykernel/modules/qca_cld
+  mv /${HOME}/kernel/zuk/anykernel/modules/wlan.ko /${HOME}/kernel/zuk/anykernel/modules/qca_cld/qca_cld_wlan.ko
+  cp -rf $objdir/arch/arm64/boot/Image.gz-dtb /${HOME}/kernel/zuk/anykernel/zImage
+}
+module_cm(){
+  rm -rf /${HOME}/kernel/zuk/anykernel/modules/
+  mkdir /${HOME}/kernel/zuk/anykernel/modules
+  find $objdir -name '*.ko' -exec cp -av {} /${HOME}/kernel/zuk/anykernel/modules/ \;
+  # strip modules
+ ${TOOL_CHAIN_PATH}/${CROSS_COMPILE}strip --strip-unneeded ${HOME}/kernel/zuk/anykernel/modules/*
+  cp -rf $objdir/arch/arm64/boot/Image.gz-dtb /${HOME}/kernel/zuk/anykernel/zImage
+}
+dtbuild(){
+  cd $sourcedir
+  ./tools/dtbToolCM -2 -o $objdir/arch/arm64/boot/dt.img -s 4096 -p $objdir/scripts/dtc/ $objdir/arch/arm64/boot/dts/
+}
+#clean
+compile
+module_stock
+#dtbuild
+#cp $objdir/arch/arm64/boot/zImage $sourcedir/zImage
+#cp $objdir/arch/arm64/boot/dt.img.lz4 $sourcedir/dt.img
---
+++ config.sh
@@ -0,0 +1,17 @@
+KERNEL_DIR=$PWD
+export ARCH=arm64
+export SUBARCH=arm64
+
+export KBUILD_BUILD_USER="Fedor917"
+export KBUILD_BUILD_HOST="Zanovo"
+
+export CROSS_COMPILE=/home/fedor917/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+#export STRIP=STRIP=/home/fedor917/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-strip
+MODULES_DIR=$KERNEL_DIR/out_modules
+
+make lineageos_z2_plus_defconfig
+#make menuconfig
+#make oldconfig
+#make -j8
+make mrproper
+
