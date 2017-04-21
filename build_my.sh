2331d481637801e4313db89a165f3d415c3f7b26
dd3boh <dade.garberi@gmail.com>
2017-04-21 23:36:26 +0200

    build_my.sh: Improvements

--- build_my.sh
+++ build_my.sh
@@ -4,12 +4,12 @@ export ARCH="arm64"
 export CROSS_COMPILE="aarch64-linux-gnu-"
 export KBUILD_BUILD_USER="haha"
 export KBUILD_BUILD_HOST="DD3Boh"
-export TOOL_CHAIN_PATH="/home/dd3/kernel/linaro-6.3-aarch64-linux/bin"
+export TOOL_CHAIN_PATH="${HOME}/kernel/linaro-6.3-aarch64-linux/bin"
 export CONFIG_ABS_PATH="arch/${ARCH}/configs/${CONFIG_FILE}"
 export PATH=$PATH:${TOOL_CHAIN_PATH}
 export objdir="${HOME}/kernel/zuk/obj"
-export sourcedir="/home/dd3/kernel/zuk/kernel"
-export anykernel="/home/dd3/kernel/zuk/anykernel"
+export sourcedir="${HOME}/kernel/zuk/kernel"
+export anykernel="${HOME}/kernel/zuk/anykernel"
 compile() {
   make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE}  $CONFIG_FILE -j4
   make O=$objdir -j6
