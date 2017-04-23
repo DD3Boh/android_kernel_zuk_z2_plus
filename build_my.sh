2f507c37ff7e56bcfc04bb40b3ec4a44a7a787fd
dd3boh <dade.garberi@gmail.com>
2017-04-23 15:49:04 +0200

    LightMoon: adapt build_my.sh

--- build_my.sh
+++ build_my.sh
@@ -7,9 +7,9 @@ export KBUILD_BUILD_HOST="DD3Boh"
 export TOOL_CHAIN_PATH="${HOME}/kernel/aarch64-linux-android-6.x/bin"
 export CONFIG_ABS_PATH="arch/${ARCH}/configs/${CONFIG_FILE}"
 export PATH=$PATH:${TOOL_CHAIN_PATH}
-export objdir="${HOME}/kernel/zuk/darkmoon-obj"
-export sourcedir="${HOME}/kernel/zuk/DarkMoon"
-export anykernel="${HOME}/kernel/zuk/darkmoon-anykernel"
+export objdir="${HOME}/kernel/zuk/lightmoon-obj"
+export sourcedir="${HOME}/kernel/zuk/LightMoon"
+export anykernel="${HOME}/kernel/zuk/lightmoon-anykernel"
 compile() {
   make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE}  $CONFIG_FILE -j4
   make O=$objdir -j6
